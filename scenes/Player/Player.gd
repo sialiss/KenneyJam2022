266a5b86f4cc50e236e2c3f6a683c408530dae32
och-och <och.hco.och.hco@gmail.com>
2022-08-20 19:14:54 +0300

    Tweak Movement

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -3,17 +3,21 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var max_speed = 16*10
-var acceleration_time = 0.05
+var acceleration_time = 0.1
 var acceleration = max_speed / acceleration_time
 var deceleration_time = 0.3
 var deceleration = max_speed / deceleration_time
 var gravity = 16*60 / 1
+var jump_speed = 16*20
 
-var under_max_speed = 16*12
-var under_acceleration_time = 0.05
+var under_max_speed = 16*15
+var under_acceleration_time = 0.1
 var under_acceleration = max_speed / under_acceleration_time
 var under_deceleration_time = 0.1
 var under_deceleration = max_speed / under_deceleration_time
+
+var burrow_speed = 16*20
+var unburrow_speed = 16*20
 
 
 onready var UndergroundSurfaceDetector = $"%UndergroundSurfaceDetector"
@@ -36,10 +40,13 @@ class OvergroundStatus extends Status:
 		else:
 			host.velocity.x = move_toward(host.velocity.x, 0, host.deceleration * delta)
 
+		if Input.is_action_just_pressed("jump") and host.is_on_floor():
+			host.velocity.y -= host.jump_speed
+
 		host.velocity = host.move_and_slide_with_snap(host.velocity, Vector2.DOWN, Vector2.UP)
 
 
-		if Input.is_action_just_pressed("move_down"):
+		if Input.is_action_just_pressed("burrow"):
 			DigDown.new().attach(host)
 
 
@@ -64,7 +71,14 @@ class DigDown extends Status:
 
 	func _physics_process(delta):
 		# Gravity
-		host.velocity.y += host.gravity * delta
+		host.velocity.y = move_toward(host.velocity.y, host.burrow_speed, host.gravity * delta)
+
+		# Movement
+		var input = Input.get_axis("move_left", "move_right")
+		if input != 0:
+			host.velocity.x = move_toward(host.velocity.x, input * host.max_speed, host.acceleration * delta)
+		else:
+			host.velocity.x = move_toward(host.velocity.x, 0, host.deceleration * delta)
 
 		host.velocity = host.move_and_slide(host.velocity, Vector2.UP)
 
@@ -74,7 +88,14 @@ class DigDown extends Status:
 class DigUp extends Status:
 	func _physics_process(delta):
 		# Gravity
-		host.velocity.y -= host.gravity * delta
+		host.velocity.y = move_toward(host.velocity.y, -host.unburrow_speed, host.gravity * delta)
+
+		# Movement
+		var input = Input.get_axis("move_left", "move_right")
+		if input != 0:
+			host.velocity.x = move_toward(host.velocity.x, input * host.max_speed, host.acceleration * delta)
+		else:
+			host.velocity.x = move_toward(host.velocity.x, 0, host.deceleration * delta)
 
 		host.velocity = host.move_and_slide(host.velocity, Vector2.UP)
 
