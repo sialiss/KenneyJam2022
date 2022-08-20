34c9cf34e3407a595f76d9c266142f4e3cf12001
och-och <och.hco.och.hco@gmail.com>
2022-08-20 22:20:08 +0300

    Add Stat Resource

---
+++ Utils/Stats/Stat.gd
@@ -0,0 +1,15 @@
+tool
+extends Resource
+class_name Stat
+
+export(float) var value = 0.0 setget set_value
+export(float) var maximum = 0.0 setget set_maxumum
+
+
+func set_value(val):
+	value = clamp(val, 0, maximum)
+	resource_name = "%d/%d" % [value, maximum]
+
+func set_maxumum(val):
+	maximum = val
+	self.value = value
