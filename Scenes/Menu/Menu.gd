25ef14f1294077eb392714120a0a2406e704525d
Camellia <56206613+sialiss@users.noreply.github.com>
2022-08-21 15:50:26 +0300

    Add menu and credits scenes, make game theme

---
+++ Scenes/Menu/Menu.gd
@@ -0,0 +1,21 @@
+extends CanvasLayer
+
+var game_scene = load("res://scenes/Main.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# $MenuMusic.play()
+	$Credits.hide()
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+
+func _on_StartButton_pressed():
+	get_tree().change_scene_to(game_scene)
+
+func _on_CreditsButton_pressed():
+	$Credits.show()
+
+func _on_Credits_go_to_menu():
+	$Credits.hide()
